fb44b106d2be4d47e70bce17776d4b0950dba531
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-05-03 19:57:11 +0800

    Revert "Update and test signin route handler with mongoose"
    This reverts commit 556db55638049bd6ebd671b0265a9e4c06980f4d.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { User } from '../models/user';
+import { DatabaseConnectionError } from '../errors/database-validation-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
@@ -14,21 +14,12 @@ router.post('/api/users/signup', [
         .isLength({min:4, max: 20})
         .withMessage('Password must be between 4 and 20 characters')
 ],
-async (req: Request,res: Response)=>{
+(req: Request,res: Response)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
-    const {email, password} = req.body;
-    const existingUser = await User.findOne({email});
-    if (existingUser) {
-        return res.send({});
-    }
-    const user = User.build({email,password});
-    try {
-        await user.save();
-        res.status(201).send(user);
-    } catch (err) {
-        res.send(err);
-    }
+    console.log('Creating a user...')
+    throw new DatabaseConnectionError();
+    res.send('Signed up!')
 })
 
 export {router as signUpRouter};
