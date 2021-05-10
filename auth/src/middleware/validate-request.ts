fd6428721435952051f6ff7ea5f2ea9cf531660d
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-05-10 16:46:25 +0800

    Updated middlewares for less code duplication

--- auth/src/middleware/validate-request.ts
+++ auth/src/middleware/validate-request.ts
@@ -2,7 +2,6 @@ import {Request, Response, NextFunction} from 'express';
 import { validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 export const validateRequest = (
-    err: Error,
     req: Request,
     res: Response,
     next: NextFunction,
