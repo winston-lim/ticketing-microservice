eef9bdd2e52f3661e4bd63580ad7ccc64ea9ab1e
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-05-11 11:39:27 +0800

    Refactor express set up for testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signOutRouter } from './routes/signout';
-import  { signUpRouter } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import cookieSession from 'cookie-session';
-
-const app = express();
-app.set('trust proxy', true); //traffic is being proxied to our app throug ingress-nginx, without this setting, express does not trust the HTTPS connection
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false, //no encryption on cookie content
-    secure: true,  //require a HTTPS connection for this route
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('/*', ()=> {
-  console.log('No such route')
-  throw new NotFoundError();
-})
-app.use(errorHandler);
-
+import {app} from './app';
 const start = async ()=> {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined')
