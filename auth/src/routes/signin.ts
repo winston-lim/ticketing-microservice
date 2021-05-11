638c6334ed67fd51c500223cf0c7487a526e5413
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-05-11 11:39:05 +0800

    Update various routes with authentication middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middleware/validate-request';
+import { User } from '../models/user';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Signed in current user');
+router.post('/api/users/signin', [
+  body('email')
+    .isEmail()
+    .withMessage('Enter a valid email'),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Enter a password')
+],
+validateRequest,
+async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await User.login(email, password);
+  const userJwt = user.generateAuthToken();
+  req.session = {
+    jwt: userJwt,
+  }
+  res.send(user);
 });
 
 export { router as signInRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middleware/validate-request';
 import { DatabaseConnectionError } from '../errors/database-validation-error';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
+import {User} from '../models/user';
+import { validateLocaleAndSetLanguage } from 'typescript';
 
 const router = express.Router();
 
@@ -13,13 +18,29 @@ router.post('/api/users/signup', [
         .trim()
         .isLength({min:4, max: 20})
         .withMessage('Password must be between 4 and 20 characters')
-],
-(req: Request,res: Response)=>{
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
-    console.log('Creating a user...')
-    throw new DatabaseConnectionError();
-    res.send('Signed up!')
+], 
+validateRequest,
+async (req: Request,res: Response)=>{
+    const {email, password} = req.body;
+    const existingUser = await User.findOne({email});
+    if (existingUser) {
+        console.log('Email in use')
+        throw new BadRequestError('Email in use');
+    }
+    const user = User.build({email, password});
+    console.log('user', user)
+    try {
+        console.log('saving user...')
+        await user.save();
+        const userJwt = user.generateAuthToken();
+        req.session = {
+            jwt: userJwt,
+        }
+        res.send(user);
+    } catch(err) {
+        console.log('Database Error')
+        throw new DatabaseConnectionError();
+    }
 })
 
 export {router as signUpRouter};
