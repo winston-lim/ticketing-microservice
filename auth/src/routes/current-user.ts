5f8088fb00dc43bb6df9ab4f8c908728be0565db
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-05-11 11:38:20 +0800

    Add current-user middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
-import express from 'express'
+import express from 'express';
+import { currentUser } from '../middleware/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req,res)=>{
-    res.send('Fetched current user')
+router.get('/api/users/currentuser', currentUser, (req,res)=>{
+    res.send({ currentUser: req.currentUser || null })
 })
 
 export {router as currentUserRouter};
