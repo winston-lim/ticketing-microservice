cec77086095fa7ae743597e88527ef2500f58358
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-05-14 10:46:10 +0800

    Build custom useRequest hook

---
+++ client/hooks/use-request.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const useRequest =  ({ url, method, body, onSuccess}) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url,body);
+      if (onSuccess) onSuccess(response.data);
+      return response.data;
+    } catch (e) {
+      setErrors(
+        <div className="alert alert-danger">
+        <h4>Error:</h4>
+        <ul className="my-0">
+        {e.response.data.map(err=><li key={err.message}>{err.message}</li>)}
+        </ul>
+      </div>
+      )
+    }
+  }
+
+  return { errors, doRequest }
+}
+
+export default useRequest;
