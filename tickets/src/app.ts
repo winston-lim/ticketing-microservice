148f46b6bc0dd0f1d82f93f111edb34db6f7021d
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-05-17 11:41:18 +0800

    Create tickets express server and one route

---
+++ tickets/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'express-async-errors';
+import { errorHandler, NotFoundError, currentUser } from '@winston-test/common';
+import cookieSession from 'cookie-session';
+import { createTicketRouter } from './routes/new';
+
+const app = express();
+app.set('trust proxy', true); //traffic is being proxied to our app throug ingress-nginx, without this setting, express does not trust the HTTPS connection
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false, //no encryption on cookie content
+    secure: process.env.NODE_ENV!=='test',  //require a HTTPS connection for this route
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+
+app.all('*', ()=> {
+  throw new NotFoundError();
+})
+app.use(errorHandler);
+
+export {app};
---
+++ tickets/src/routes/new.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@winston-test/common';
+import { body } from 'express-validator';
+
+const router = express.Router();
+
+router.post('/api/tickets', requireAuth, [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('A valid title is required'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be greater than 0')
+], validateRequest, (req: Request, res: Response) => {
+
+  res.sendStatus(200);
+})
+
+export { router as createTicketRouter};
