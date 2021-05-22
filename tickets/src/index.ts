b6418c498b959a25986df97cb23987de6431116b
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-05-22 11:32:54 +0800

    Create NATS related env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -9,8 +9,17 @@ const start = async ()=> {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined')
   }
+  if (!process.env.NATS_URL) {
+    throw new Error('JWT_KEY must be defined')
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('MONGO_URI must be defined')
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('JWT_KEY must be defined')
+  }
   try {
-    await stan.connect('ticketing','randomString', 'http://nats-srv:4222');
+    await stan.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
     stan.client.on('close', ()=>{
       console.log('NATS connection closed');
       process.exit();
