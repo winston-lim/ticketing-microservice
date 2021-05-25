4fd7d744f413f167846ea8c17bd71ac10f320a6c
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-05-25 11:14:22 +0800

    Build Order model

---
+++ orders/src/models/order.ts
@@ -0,0 +1,53 @@
+import mongoose, { Document, Schema, Model} from 'mongoose';
+import { OrderStatus } from '@winston-test/common';
+import { TicketDoc } from './ticket';
+export interface baseOrder {
+    userId: string;
+    status: OrderStatus;
+    expiresAt: Date;
+    ticket: TicketDoc;
+}
+
+interface baseOrderDocument extends baseOrder, Document{
+    createdBy: string;
+    updatedAt: string;
+}
+
+interface OrderDocument extends baseOrderDocument {
+}
+
+export interface OrderModel extends Model<OrderDocument>{
+  build(ticket: baseOrder): OrderDocument;
+}
+
+const orderSchema = new Schema<OrderDocument, OrderModel>({
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus)
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket'
+    }
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id=ret._id;
+            delete ret._id;
+        }
+    }
+})
+
+orderSchema.statics.build = (order: baseOrder) => {
+  return new Order(order);
+}
+
+export const Order =  mongoose.model<OrderDocument, OrderModel>('Order', orderSchema);
