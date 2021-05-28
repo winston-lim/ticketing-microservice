a8c8a03c88acf13c2d2bb5cc023227f978099df6
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-05-28 15:36:32 +0800

    Write concurrency and versioning tests in listeners

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,42 @@
+import { TicketCreatedEvent } from '@winston-test/common';
+import { Message, Stan } from 'node-nats-streaming';
+import { Types } from 'mongoose';
+import stan from "../../../__mocks__/stan";
+import { TicketCreatedListener } from "../ticket-created-listener";
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  const listener = new TicketCreatedListener(stan.client as unknown as Stan);
+  const data: TicketCreatedEvent['data'] = {
+    id: new Types.ObjectId().toHexString(),
+    version: 0,
+    title: 'test',
+    price: 20,
+    userId: new Types.ObjectId().toHexString(), 
+  }
+
+  const msg: Partial<Message> = {
+    ack: jest.fn(),
+  }
+  return {
+    listener,
+    data,
+    msg
+  }
+}
+
+it('creates and save a ticket', async ()=> {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data,msg as Message);
+  
+  const ticket = await Ticket.findById(data.id);
+  expect(ticket).not.toBeNull();
+  expect(ticket!.title).toEqual(data.title);
+  expect(ticket!.price).toEqual(data.price);
+});
+
+it('acks the message', async ()=> {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg as Message);
+  expect(msg.ack).toBeCalledTimes(1);
+});
---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,64 @@
+import { Message, Stan } from 'node-nats-streaming';
+import { TicketCreatedEvent as TicketUpdatedEvent } from "@winston-test/common";
+import { Types } from "mongoose";
+import stan from "../../../__mocks__/stan";
+import { Ticket } from '../../../models/ticket';
+import { TicketUpdateListener } from '../ticket-updated-listener';
+
+
+const setup = async () => {
+  const listener = new TicketUpdateListener(stan.client as unknown as Stan);
+
+  const ticketId = new Types.ObjectId().toHexString();
+  const ticket = Ticket.build({
+    id: ticketId,
+    title: 'test',
+    price: 20,
+  });
+  await ticket.save();
+  console.log ('saved', ticket);
+
+  const data: TicketUpdatedEvent['data'] = {
+    id: ticketId,
+    version: ticket.version + 1,
+    title: 'updated title',
+    price: 21,
+    userId: new Types.ObjectId().toHexString(),
+  }
+
+  const msg: Partial<Message> = {
+    ack: jest.fn(),
+  }
+  return {
+    listener,
+    data,
+    msg
+  }
+}
+
+it('finds, updates and saves a ticket', async ()=> {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg as Message);
+  
+  const updatedTicket = await Ticket.findById(data.id);
+  expect(updatedTicket!.title).toEqual(data.title);
+  expect(updatedTicket!.price).toEqual(data.price);
+  expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it('acks the message on applying a update successfully', async ()=> {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg as Message);
+  expect(msg.ack).toHaveBeenCalledTimes(1);
+});
+
+it('does not ack the message when an invalid update is applied', async () => {
+  const { listener, data: { id, title, price, userId }, msg } = await setup();
+  try {
+    await listener.onMessage({ id, title, price, userId, version: 100} , msg as Message); 
+  } catch (e) {
+
+  }
+
+  expect(msg.ack).not.toHaveBeenCalled();
+})
