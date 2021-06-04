67b6cfb72e975cebd3f052286199a17be29a9cce
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-06-04 18:22:18 +0800

    Refactor code

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 import {app} from './app';
 const start = async ()=> {
-  console.log('Starting auth service...')
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined')
   }
