4ca591f34a152781106d0297938856a3dd5513e8
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-06-04 15:42:46 +0800

    Update landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,21 +1,40 @@
-import buildClient from '../api/build-client';
+import Link from "next/link"
 
-const Landing = ({ currentUser }) => {
-  return currentUser 
-    ?
-      <h1>
-        You are signed in
-      </h1> 
-    :
-      <h1>
-        You are NOT signed in
-      </h1>
+const Landing = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map(ticket=> {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href='/tickets/[ticketId]' as={`/tickets/${ticket.id}`}>
+            <a>View Ticket</a>
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+        {ticketList}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-Landing.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const response = await client.get('/api/users/currentuser');
-  return response.data;
+Landing.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+  return { tickets: data };
 }
 
 export default Landing;
