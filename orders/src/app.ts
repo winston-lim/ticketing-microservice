fcc683d20d1c68337c542fd619229b197106f2e7
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-06-05 16:55:37 +0800

    Fix various bugs for deployment on DigitalOcean

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
   cookieSession({
     signed: false, //no encryption on cookie content
-    secure: process.env.NODE_ENV!=='test',  //require a HTTPS connection for this route
+    secure: false,
   })
 );
 app.use(currentUser);
